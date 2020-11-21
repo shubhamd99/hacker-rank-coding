cb1cb7a6385c5f230308b027a5cf4cb75e3eed08
Shubham Dhage <shubhamd@rigbot.com>
2020-11-21 15:08:37 +0530

    add faster solution for frequency queries problem

--- PreparationKit/DictionariesAndHashmaps/FrequencyQueries.js
+++ PreparationKit/DictionariesAndHashmaps/FrequencyQueries.js
@@ -19,8 +19,48 @@
 
 // Return an array with the output: [0,1].
 
+
 // Complete the freqQuery function below. (Little Slow, 1 test failed out of all)
 function freqQuery(queries) {
+
+    const frequencies = [];
+    const frequencyTracker = [];
+    const result = [];
+
+    for (const query of queries) {
+        const action = query[0];
+        const value = query[1];
+        let index;
+
+        if (action === 1 || action === 2) {
+            index = frequencies[value];
+            frequencyTracker[index] ? --frequencyTracker[index] : null;
+        }
+
+        if (action === 1) {
+            typeof frequencies[value] === 'undefined' ? frequencies[value] = 1 : ++frequencies[value];
+        }
+
+        if (action === 2 && frequencies[value]) {
+            --frequencies[value];
+        }
+
+        if (action === 1 || action === 2) {
+            index = frequencies[value];
+            frequencyTracker[index] ? ++frequencyTracker[index] : frequencyTracker[index] = 1;
+        }
+
+        if (action === 3) {
+            result.push(frequencyTracker[value] > 0 ? 1 : 0);
+        }
+    }
+
+    return result;
+
+}
+
+// Complete the freqQuery function below. (Little Slow, 1 test failed out of all)
+function freqQuery2(queries) {
     let dataStructure = [];
     let charMap = {};
     let output = [];
